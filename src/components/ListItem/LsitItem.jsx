d9e558d0b05cc8fa429513e5dbf95d02f885327f
ahmed <AhmedNnasser11>
2022-04-10 14:24:07 +0200

    my changes

--- src/components/ListItem/LsitItem.jsx
+++ src/components/ListItem/LsitItem.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./ListItem.scss";
-import { FaCheck, FaTrashAlt, FaRegTimesCircle } from "react-icons/fa";
+import { FiCheckSquare } from "react-icons/fi";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { BsTrash } from "react-icons/bs";
+
 const LsitItem = (props) => {
   const { title, completed, id } = props.todo;
   const [isCompleted, setIsCompleted] = useState(completed)
@@ -18,11 +21,11 @@ const LsitItem = (props) => {
     <div className={`item ${isCompleted && "completed"}`}>
       <span className="icons">
         {isCompleted ? (
-          <FaCheck className="check" onClick={toggleCompleted} />
+          <FiCheckSquare className="check" onClick={toggleCompleted} />
         ) : (
-          <FaRegTimesCircle className="close" onClick={toggleCompleted} />
+          <MdCheckBoxOutlineBlank className="close" onClick={toggleCompleted} />
         )}
-        <FaTrashAlt onClick={handlerDelete} className='delete'/>
+        <BsTrash onClick={handlerDelete} className='delete'/>
       </span>
       <p>{title}</p>
     </div>
